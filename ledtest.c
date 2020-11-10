8958fdbc271789f85833c0b00f177702f88f81f6
Shim jeong hwan <micron826@mju.ac.kr>
2020-11-11 01:10:46 +0900

    ledtest.c 업데이트
    수정완

--- ledtest.c
+++ ledtest.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include "led.h"
 
 int main(void){
     
+    ledLibInit();
+
+    ledLibRaw(0xFF);
+    sleep(1);
+    ledLibRaw(0xAA);
+    sleep(1);
+    ledLibRaw(0x55);
+    sleep(1);
+    ledLibRaw(0x00);
+    
+    int i  = 0;
+    for(i = 0; i < 8; i++){
+        ledLibOnOff(i , 1);
+        pritnf("0x%02X--ok?\r\n", ledLibStatus());
+        sleep(1);
+        ledLibOnOff(i , 0);
+        sleep(1);
+    }
+
+    ledLiBlink(5, 10, 500);
+
+    ledLibMorseBlink(2, 3, 500, 500);
+    ledLibMorseBlink(2, 3, 1500, 500);
+    ledLibMorseBlink(2, 3, 1500, 500);
+
+    sleep(1);
+    ledLibExit();
 }
