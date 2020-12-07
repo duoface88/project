81ea11b0669c6a83e0ccc3c2fd35abe09afbc51c
KimJunYoung8795 <72500089+KimJunYoung8795@users.noreply.github.com>
2020-12-07 13:36:31 +0900

    Update accelMagGyro.h

--- accelMagGyro.h
+++ accelMagGyro.h
@@ -7,11 +7,6 @@
 	int curMag(void);
 	int curGyro(void);
 	
-typedef struct FNDWriteDataForm_tag
-{
-char DataNumeric[FND_DATA_BUFF_LEN]; //숫자 0-9
-char DataDot[FND_DATA_BUFF_LEN]; //숫자 0 or 1
-char DataValid[FND_DATA_BUFF_LEN]; //숫자 0 or 1
-}stFndWriteForm,*pStFndWriteForm;
-int fndDisp(int num, int dotflag);
+
+
 #endif __ACCELMAGGYRO_H__
